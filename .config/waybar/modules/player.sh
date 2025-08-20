853bb45038425bf2f51e6c49d771bb89a5c4a627
Jelly_Man <castillosant19@protonmail.com>
2025-08-20 15:28:25 -0600

    Changes

--- .config/waybar/modules/player.sh
+++ .config/waybar/modules/player.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-class=$(playerctl metadata -a --format '{{lc(status)}}')
+class=$(playerctl metadata -p mpd --format '{{lc(status)}}')
 
 if [[ $class == "playing" ]]; then
-  info=$(playerctl metadata -a --format '{{artist}} - {{title}}')
+  info=$(playerctl metadata -p mpd --format '{{artist}} - {{title}}')
   icon=""
   if [[ ${#info} > 40 ]]; then
-    info=$(echo $info | cut -c1-30)"..."
+    info=$(echo $info | cut -c 1-30)"..."
   fi
   text=$icon"  "$info
 elif [[ $class == "paused" ]]; then
-  info=$(playerctl metadata -a --format '{{artist}} - {{title}}')
+  info=$(playerctl metadata -p mpd --format '{{artist}} - {{title}}')
   icon=""
   if [[ ${#info} > 40 ]]; then
-    info=$(echo $info | cut -c1-30)"..."
+    info=$(echo $info | cut -c 1-30)"..."
   fi
   text=$icon"  "$info
 elif [[ $class == "stopped" ]]; then
